5ddde0ab1e0afd44da4cd40d66a1a8866733058c
spanri <kozlova9v@mail.ru>
2023-01-05 22:11:53 +0300

    Чуть правка настроек

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -6,6 +7,13 @@ export default defineConfig(({ mode }) => {
   return {
     root: "",
     base: env.VITE_BASE_URL,
+    rewriteUrls: 'all',
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./public/"),
+        "~@": path.resolve(__dirname, "./public/"),
+      },
+    },
     server: {
       host: true,
       port: 8080,
